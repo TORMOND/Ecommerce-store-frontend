369d6f546f67a3c4ffd260aa523b1707abf6a053
victormonderu@gmail.com <victormonderu@gmail.com>
2023-08-02 01:48:42 +0300

    changed wrong path

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,7 +2,7 @@ import furniture from './furniture.jpg';
 import medicalEquipment  from './medical-equipment.jpg';
 import stationary from './stationary.jpg'
 import clothes from './clothes.jpg'
-import  buildingmaterials  from './building-materials-2.jpg';
+import  buildingmaterials  from './Building-materials-2.jpg';
 import safetyTools from './safety-tools.jpg'
 import solar from './solar.jpg'
 import fertilizers from './fertilizers.jpg'
